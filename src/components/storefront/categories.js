fa98eaccc9c97139cf14b3df508da19298d5b0fe
Davee Sok <daveesok@gmail.com>
2020-12-10 17:14:26 -0800

    updated category and current category

--- src/components/storefront/categories.js
+++ src/components/storefront/categories.js
@@ -1,8 +1,32 @@
-function Categories() {
-  return (
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
 
-    <h1> Categories </h1>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function Categories() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+
+      <h2>Browse Our Categories</h2>
+      <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+        <Button>Food</Button>
+        <Button>Categories</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+
+    </div>
   );
 }
-
-export default Categories;
--- src/components/storefront/current-category.js
+++ src/components/storefront/current-category.js
@@ -1,7 +1,41 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  fullHeight: {
+    height: "100%"
+  },
+}));
+
 function CurrentCategories() {
+
+  const classes = useStyles();
+
   return (
 
-    <h1> CurrentCategories </h1>
+    <>
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          Food
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Food Description
+        </Typography>
+      </Container>
+    </>
+
   );
 }
 
